d37a1a7cc9f4948fa7ba52df886aaac90254c05d
Herdeson <herdeson.1@gmail.com>
2017-07-06 12:50:37 -0300

    Alterando o meus dados

--- accounts/views.py
+++ accounts/views.py
@@ -1,8 +1,14 @@
 from django.shortcuts import render
-from django.views.generic import CreateView
+from django.views.generic import CreateView, TemplateView, UpdateView
 from django.core.urlresolvers import reverse_lazy
 from .models import User
 from .forms import UserAdminCreatForm
+
+from django.contrib.auth.mixins import LoginRequiredMixin
+
+# Usado quando for uma view baseada em função
+# from django.contrib.auth.decorators import login_required
+
 # Create your views here.
 
 class RegisterView(CreateView):
@@ -12,3 +18,20 @@ class RegisterView(CreateView):
     success_url = reverse_lazy('index')
 
 register = RegisterView.as_view()
+
+# A ordem é importante
+class IndexView( LoginRequiredMixin , TemplateView):
+    template_name = 'accounts/index.html'
+
+index = IndexView.as_view()
+
+class UpdateUserView(LoginRequiredMixin, UpdateView):
+    model = User
+    template_name = 'accounts/update_user.html'
+    fields = ['name', 'email']
+    success_url = reverse_lazy('accounts:index')
+
+    def get_object(self):
+        return self.request.user
+
+update_user = UpdateUserView.as_view()
